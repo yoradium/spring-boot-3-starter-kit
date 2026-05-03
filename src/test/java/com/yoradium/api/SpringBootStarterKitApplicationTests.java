4841ef6992c9a8a65e147a0d89b573ccf6be6591
yoradium <yoradium@gmail.com>
2026-05-03 17:45:21 +0200

    test: removed contextLoads test

--- src/test/java/com/yoradium/api/SpringBootStarterKitApplicationTests.java
+++ src/test/java/com/yoradium/api/SpringBootStarterKitApplicationTests.java
@@ -1,13 +1,7 @@
 package com.yoradium.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootStarterKitApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
